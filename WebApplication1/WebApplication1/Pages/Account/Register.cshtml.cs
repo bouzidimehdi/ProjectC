ebaf0bf50e16a91234e39786229c8cc63d8f479d
Mehdi <mehdibouzidi@live.nl>
2018-10-02 15:08:16 +0200

    Project

--- WebApplication1/WebApplication1/Pages/Account/Register.cshtml.cs
+++ WebApplication1/WebApplication1/Pages/Account/Register.cshtml.cs
@@ -42,6 +42,7 @@ namespace WebApplication1.Pages.Account
             [StringLength(50, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 3)]
             [DataType(DataType.Text)]
             [Display(Name = "Name")]
+            [RegularExpression(@"^[A-Za-zÀ-ÿ]+$", ErrorMessage = "Please only enter letters")]
             public string Name { get; set; }
 
             [Required]
