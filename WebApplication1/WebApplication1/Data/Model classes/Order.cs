2b1a6b07128cf0044c7c76efaef73bfafc905405
Dion <dion_1997@hotmail.nl>
2018-10-07 17:09:46 +0200

    Added database tables.
    Voeg alle tablen opnieuw toe na merge conflicten

---
+++ WebApplication1/WebApplication1/Data/Model classes/Order.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace WebApplication1.Data
+{
+    public class Order
+    {
+        public int ID { get; set; }
+        public string User_ID { get; set; }
+        public int Factuur_ID { get; set; }
+        public ApplicationUser User { get; set; }
+        public Factuur Factuur { get; set; }
+        public List<Orderd_Product> OrderdProducts { get; set; }
+    }
+}
