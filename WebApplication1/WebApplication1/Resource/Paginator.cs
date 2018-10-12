65a62ae8ef8e863bb2664319ec2e0bf892697f0f
Dion <dion_1997@hotmail.nl>
2018-10-12 16:57:00 +0200

    Searchbar function
    Searchbar functie die de zoek opdracht filter en de meest relevante zoek opdracht boven aan plaats.

---
+++ WebApplication1/WebApplication1/Resource/Paginator.cs
@@ -0,0 +1,18 @@
+﻿namespace WebApplication1.Paginator
+{
+    //a page-datastructure used to limit the amount of data
+    //to return to the client. This data-structure comes to help
+    //selecting large amount of entities from the database
+    public class Page<T>
+    {
+
+        // index of the current selected page
+        public int Index { get; set; }
+
+        // amount of entities per page
+        public T[] Items { get; set; }
+
+        // Total pages
+        public int TotalPages { get; set; }
+    }
+}
