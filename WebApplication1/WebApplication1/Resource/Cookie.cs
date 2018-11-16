4135f08fd1eb484a43a1fc2b9c006e1a2ce7866d
Dion <dion_1997@hotmail.nl>
2018-11-16 10:53:59 +0100

    Shopping cart unregisterd user working.
    Shopping cart for unregisterd user is working.
    Only delete function is not yet working.

---
+++ WebApplication1/WebApplication1/Resource/Cookie.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+
+namespace WebApplication1.Resource
+{
+    public static class Cookie
+    {
+        public static List<shoppingCart_cookie> Cookiereader_shoppingcart(string cookieshoping)
+        {
+            List<shoppingCart_cookie> shoppingcartlist = new List<shoppingCart_cookie>();
+
+            if (cookieshoping != null)
+            {
+                string[] shoppingCartCookies1 = Splitter.stringsplitter(cookieshoping, "_");
+
+                foreach (string item in shoppingCartCookies1)
+                {
+                    string[] result = item.Split("-");
+                    if (result.Length == 2)
+                    {
+                        shoppingcartlist.Add(new shoppingCart_cookie()
+                            {ProductID = int.Parse(result[0]), Quantity = int.Parse(result[1])});
+                    }
+                }
+            }
+
+            return shoppingcartlist;
+        }
+
+    }
+
+    public class shoppingCart_cookie
+    {
+        public int ProductID { get; set; }
+        public int Quantity { get; set; }
+    }
+}
