a7b482cda5beb83e007101706615e89d61bf6748
TimOosterlee <0929381@hr.nl>
2018-11-20 13:02:46 +0100

    Adminpanel- Edit user
    nog niet werkend

---
+++ WebApplication1/WebApplication1/Pages/Account/Manage/TestEdit.cshtml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Data;
+
+namespace WebApplication1.Pages.Account.Manage
+{
+    public class TestEditModel : PageModel
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+        public TestEditModel(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        [BindProperty]
+        public ApplicationUser Users { get; set; }
+        public async Task<IActionResult> OnGet(string id)
+        {
+            var user = _userManager.FindByIdAsync(id);
+            Users = await _context.Users.FindAsync(user);
+            if(Users == null)
+            {
+                return RedirectToPage("Account/Manage/Admin");
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            
+            
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            _context.Attach(Users).State = EntityState.Modified;
+            //Users.ConcurrencyStamp = Guid.NewGuid().ToString();
+            
+
+            try
+            {
+                await _userManager.UpdateAsync(Users);
+            }
+            catch (DbUpdateConcurrencyException e)
+            {
+                throw new Exception($"User {Users.Id} not found!", e);
+            }
+
+            return RedirectToPage("Account/Manage/Admin");
+        }
+    }
+}
