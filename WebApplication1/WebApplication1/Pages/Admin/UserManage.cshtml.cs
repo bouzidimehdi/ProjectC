df259432a1dbd70566356b4ae2ac589d9f100baf
Dion <dion_1997@hotmail.nl>
2018-11-28 14:22:12 +0100

    Give admin the ability to create an user.
    Create User is working.

--- WebApplication1/WebApplication1/Pages/Admin/UserManage.cshtml.cs
+++ WebApplication1/WebApplication1/Pages/Admin/UserManage.cshtml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
@@ -12,34 +13,55 @@ namespace WebApplication1.Pages.Admin
     public class UserManageModel : PageModel
     {
         private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
 
         // all users
         public IList<ApplicationUser> Users { get; private set; }
-        // all products
-        public IList<Product> _Products { get; set; }
 
-        public UserManageModel(ApplicationDbContext context)
+        public UserManageModel(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
         {
+            _userManager = userManager;
             _context = context;
+            Users = _context.Users.AsNoTracking().ToList();
         }
         public string Message { get; set; }
 
-        public async Task OnGetAsync(int productid)
+        public void OnGet()
         {
-            //var query = (from alluser in _context.Users
-            //             select alluser).ToList();
-            //_users = query;
-
-            Users = await _context.Users.AsNoTracking().ToListAsync();
-
-            // get products ( test )
-            var query = (from products in _context.Product
-                where products.ID > 13160
-                select products).ToList();
-
-            _Products = query;
-
             Message = "Your application description page.";
+        }
+
+        public void OnGetDelete(string UserID)
+        {
+
+            ApplicationUser user = _userManager.FindByIdAsync(UserID).Result;
+            Shopping_card Shopping_card = _context.Shopping_card.Where(shoping => shoping.User_ID == UserID)
+                                          .FirstOrDefault();
+            if (Shopping_card != null)
+            {
+                List<Shopping_card_Product> Shopping_card_Products = _context.Shopping_Card_Products
+                    .Where(product => product.Shopping_card_ID == Shopping_card.ID)
+                    .ToList();
+
+                if (Shopping_card_Products.Count() > 0)
+                {
+                    // Remove all items from the users shoppingcard.
+                    foreach (var item in Shopping_card_Products)
+                    {
+                        _context.Shopping_Card_Products.Remove(item);
+                    }
+                }
+
+                // Remove the shoppingcard of the user.
+                _context.Shopping_card.Remove(Shopping_card);
+            }
+            
+
+            // Remove the user from database.
+            var task = Task.Run(async () => { await _userManager.DeleteAsync(user); });
+            task.Wait();
+
+            Users = _context.Users.AsNoTracking().ToList();
         }
     }
 }
