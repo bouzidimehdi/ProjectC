edabf0da2caaaa0948216586abc105ccbaf83fb7
Dion <dion_1997@hotmail.nl>
2018-12-09 15:32:31 +0100

    Email extensions
    Email extension toegevoegd.

--- WebApplication1/WebApplication1/Extensions/EmailSenderExtensions.cs
+++ WebApplication1/WebApplication1/Extensions/EmailSenderExtensions.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text.Encodings.Web;
 using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.CSharp;
+using WebApplication1.Pages;
 using WebApplication1.Services;
 
 namespace WebApplication1.Services
@@ -19,6 +21,21 @@ namespace WebApplication1.Services
         {
             return emailSender.SendEmailAsync(email, "Reset Password",
                 $"Please reset your password by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
+        }
+
+        public static Task SendKeysToEmailAsync(this IEmailSender emailSender, string email, EmailKeyArray[] EmailKey, string firstname, string lastname)
+        {
+            string listofKeys = "";
+
+            foreach (var Key in EmailKey)
+            {
+                listofKeys = listofKeys + Key.Key + Key.ProductName + "<br>";
+            }
+
+            return emailSender.SendEmailAsync(email, "Youre purchase",
+                "Thank you for youre purchase by Monkey.<br>" +
+                "Youre key's will be listed below:<br>" +
+                listofKeys);
         }
     }
 }
