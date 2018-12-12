06dd5de4ba99f96f892c22bdf2a86032ea727dfa
Dion <dion_1997@hotmail.nl>
2018-12-12 10:00:32 +0100

    Login Remove Recaptcha code.

--- WebApplication1/WebApplication1/Pages/Account/Login.cshtml.cs
+++ WebApplication1/WebApplication1/Pages/Account/Login.cshtml.cs
@@ -2,15 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
-using System.Net;
-using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json.Linq;
 using WebApplication1.Data;
 
 namespace WebApplication1.Pages.Account
