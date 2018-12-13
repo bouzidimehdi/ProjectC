a16d4024632f678d4a2695609b125e71baaf8db8
Unknown <0929381@hr.nl>
2018-12-13 19:59:13 +0100

    Auto stash before merge of "Tim" and "origin/Tim"

--- WebApplication1/WebApplication1/Pages/ShoppingCart.cshtml.cs
+++ WebApplication1/WebApplication1/Pages/ShoppingCart.cshtml.cs
@@ -27,8 +27,11 @@ namespace WebApplication1.Pages
         public List<ResponseShopingCart> _Products;
 
         public Shopping_card YourCart { get; set; }
+        public int Points { get; set; }
+        public int NewPoints { get; set; }
         public void OnGet()
         {
+            Points = 100;
             if (User.Identity.IsAuthenticated)
             {
                 string id = _userManager.GetUserId(User);
@@ -86,9 +89,17 @@ namespace WebApplication1.Pages
                 _context.Shopping_Card_Products.Remove(products);
                 await _context.SaveChangesAsync();
             }
+            return Page();
+
+
+        }
+
+        public  async Task<IActionResult> OnPostIncrease(int Points)
+        {
+            Points = Points + 10;
+            NewPoints = Points;
+
             return RedirectToPage();
-
-
         }
 
     }
