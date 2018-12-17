f4be378e011c450d93489734205073aa6e9da601
TimOosterlee <0929381@hr.nl>
2018-12-17 09:35:24 +0100

    merge vanaf desktop

--- WebApplication1/WebApplication1/Pages/Index.cshtml.cs
+++ WebApplication1/WebApplication1/Pages/Index.cshtml.cs
@@ -4,14 +4,21 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using WebApplication1.Data;
 
 namespace WebApplication1.Pages
 {
     public class IndexModel : PageModel
     {
+        private readonly ApplicationDbContext _context;
+        public Product ProductDota { get; set; }
+        public IndexModel(ApplicationDbContext context)
+        {
+            _context = context;
+        }
         public void OnGet()
         {
-
+            ProductDota = _context.Product.Where(t => t.QueryName == "Worms Reloaded").SingleOrDefault();
         }
     }
 }
