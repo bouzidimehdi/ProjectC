fdcc8d7f04ae274f0c5e48715d50007f1c7e3c5a
Unknown <0929381@hr.nl>
2018-12-22 19:35:31 +0100

    Admin statistics 66% jaren is ook te zien nu.
    moet nog dagen filter maken

--- WebApplication1/WebApplication1/Pages/Admin/Admin.cshtml.cs
+++ WebApplication1/WebApplication1/Pages/Admin/Admin.cshtml.cs
@@ -13,49 +13,48 @@ using Microsoft.AspNetCore.Identity;
 namespace WebApplication1.Pages.Admin
 {
     //date time moet nog weer veranderd worden om input te laten beslissen wat je laat zien op grafieken en cards
-
+    //TODO
+    // 1. Maak grafiek voor jaar(sales en orders) en een knop of in dropdown "Total years" en pas de cards ook aan
+    // 2. Maak niew object om data voor jaar orders op te slaan
+    // 3. Maak niew object om data voor jaar Sales op te slaan
+    // 4. Maak grafiek in script met nieuwe dataset over de grafiek die al bestaat voor order en sales
     public class AdminModel : PageModel
     {
+        private readonly ApplicationDbContext _context;
+
+        // nodig voor create/update/delete algemeen scherm om naar toe te kunnen gaan.
+        // Delete
+        [TempData] public string StatusMessage1 { get; set; }
+        // create
+        [TempData] public string StatusMessage2 { get; set; }
+
+        // all registered users
+        public IList<ApplicationUser> RegisteredUsers { get; private set; }
+        // all unregistered users
+        public IList<ApplicationUser> UnregisteredUsers { get; private set; }
+        // Alle orders die ooit zijn gemaakt in de database ( alle keys)
+        public IList<Key> AllOrders { get; set; }
+        public float SumOfOrders { get; set; }
+
+        // Array met een list van keys in AllOrdersAll en prijs van de keys in AllOrdersSales voor alle maanden
+        public List<Key>[] AllOrdersAll { get; set; }
+        public List<float>[] AllOrdersSales { get; set; }
+
+        // Array met een list van keys in AllOrdersAll en prijs van de keys in AllOrdersSales voor alle jaren
+        public List<Key>[] AllOrdersYears { get; set; }
+        public List<float>[] AllOrdersSalesYears { get; set; }
+
         public AdminModel(ApplicationDbContext context)
         {
             _context = context;
         }
 
-        private readonly ApplicationDbContext _context;
-
-        // all registered users
-        public IList<ApplicationUser> RegisteredUsers { get; private set; }
-
-        // all unregistered users
-        public IList<ApplicationUser> UnregisteredUsers { get; private set; }
-
-
-        // Alle orders die ooit zijn gemaakt in de database ( alle keys)
-        public IList<Key> AllOrders { get; set; }
-        public float SumOfOrders { get; set; }
-
-        public List<Key>[] AllOrdersAll { get; set; }
-        public List<float>[] AllOrdersSales { get; set; }
-
-
         //[BindProperty] public InputModel Input { get; set; }
-
         //public class InputModel
         //{
         //    [Required]
         //    public int jaar { get; set; }
         //}
-
-        public string Message { get; set; }
-
-            // nodig voor create/update/delete algemeen scherm om naar toe te kunnen gaan.
-
-            // Delete
-            [TempData] public string StatusMessage1 { get; set; }
-
-            // create
-            [TempData] public string StatusMessage2 { get; set; }
-
             
             public async Task OnGetAsync()
             {
@@ -68,24 +67,24 @@ namespace WebApplication1.Pages.Admin
                     //    jaar = DateTime.Now.Year
                     //};
 
-                    // Haalt alle registered users op 
+                    // Haalt alle registered en niet registered users op voor grafiek UsersGraph
                     RegisteredUsers = await _context.Users.AsNoTracking().Where(t => t.EmailConfirmed == true)
                         .ToListAsync();
                     UnregisteredUsers = await _context.Users.AsNoTracking().Where(t => t.EmailConfirmed == false)
                         .ToListAsync();
 
-                    // Haal de totale ordered producten op uit de database
+                    // Haal de totale ordered producten op uit de database 
                     AllOrders = await _context.Key
                         .ToListAsync();
                     
+                   
+                   // Orders en Sales per maand voor de laatste 3 jaren
                     AllOrdersAll = new List<Key>[36];
                     AllOrdersSales = new List<float>[36];
-                    
-                    for (int i = 0; i < 12; i++)
+
+                // Dit zet de keys van dit jaar,vorig jaar, en 2 jaar geleden in 1 array die wordt gebruikt voor 3 grafieken
+                for (int i = 0; i < 12; i++)
                     {
-                        //AllOrdersAll[i] = (from key in _context.Key
-                        //    where key.OrderDate.Month == (i + 1) && key.OrderDate.Year == Input.jaar
-                        //    select key).ToList();
                         AllOrdersAll[i] = (from key in _context.Key
                                                where (key.OrderDate.Month == (DateTime.Now.AddMonths(-i).Month) &&
                                                      (key.OrderDate.Year == (DateTime.Now.Year)))
@@ -98,13 +97,10 @@ namespace WebApplication1.Pages.Admin
                                                 where (key.OrderDate.Month == (DateTime.Now.AddMonths(-i).Month) &&
                                                       (key.OrderDate.Year == (DateTime.Now.AddYears(-2).Year)))
                                                 select key).ToList();
-                }
-
+                    }
+                // Dit zet de prijs van de keys van dit jaar,vorig jaar, en 2 jaar geleden in 1 array die wordt gebruikt voor 3 grafieken
                     for (int i = 0; i < 12; i++)
                     {
-                        //AllOrdersSales[i] = (from key in _context.Key
-                        //    where key.OrderDate.Month == (i + 1) && key.OrderDate.Year == Input.jaar
-                        //    select key.Price).ToList();
                         AllOrdersSales[i] = (from key in _context.Key
                                                  where (key.OrderDate.Month == (DateTime.Now.AddMonths(-i).Month) &&
                                                        (key.OrderDate.Year == (DateTime.Now.Year)))
@@ -117,13 +113,25 @@ namespace WebApplication1.Pages.Admin
                                                   where (key.OrderDate.Month == (DateTime.Now.AddMonths(-i).Month) &&
                                                         (key.OrderDate.Year == (DateTime.Now.AddYears(-2).Year)))
                                                   select key.Price).ToList();
-                }
-                    
+                    }
 
-                
+
+                // Orders en Sales per jaar voor de laatste 10 jaar
+                AllOrdersYears = new List<Key>[12];
+                AllOrdersSalesYears = new List<float>[12];
+
+                for (int i = 0; i < 12; i++)
+                {
+                    AllOrdersYears[i] = (from key in _context.Key
+                                           where key.OrderDate.Year == DateTime.Now.AddYears(-i).Year
+                                           select key).ToList();
+                    AllOrdersSalesYears[i] = (from key in _context.Key
+                                             where key.OrderDate.Year == DateTime.Now.AddYears(-i).Year
+                                             select key.Price).ToList();
                 }
 
-                
+                // einde van de OnGet
+            }
             }
         }
     }
